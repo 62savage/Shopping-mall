c1506804e036b06ff65e4dd88aaf8b1156971208
62savage <6s2avage@gmail.com>
2024-03-28 01:49:19 +0900

    API를 불러오기

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ProductCard() {
+  return (
+    <div>
+      <img
+        src="https://noona-hnm.netlify.app/pattern-jacket.jpeg"
+        alt="clothes"
+      />
+      <div>벨티드 트윌 코트</div>
+      <div>₩99000</div>
+      <div>신제품</div>
+    </div>
+  );
+}
--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ProductAll() {
-  return <div></div>;
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = "http://localhost:3004/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return <div>{}</div>;
 }
