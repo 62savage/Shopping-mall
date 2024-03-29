cf11131157960a84a4899532284f49874d4b0a36
62savage <6s2avage@gmail.com>
2024-03-30 01:38:48 +0900

    로그인과 로그아웃 간단하게 만들기

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ authenticate, setAuthenticate }) {
   const navigate = useNavigate();
   const menuLists = ["여성", "남성", "신생아/유아", "아동", "H&M Home", "Sale"];
 
@@ -14,18 +14,28 @@ export default function Navbar() {
       navigate(`/?q=${keyword}`);
     }
   };
+
   return (
     <div>
       <div
         className="login-button"
         onClick={() => {
+          if (authenticate) {
+            setAuthenticate(false);
+            return;
+          }
           navigate("/login");
         }}
       >
         <FontAwesomeIcon icon={faUser} />
-        <div>로그인</div>
+        <div>{authenticate ? "로그아웃" : "로그인"}</div>
       </div>
-      <div className="nav-section">
+      <div
+        className="nav-section"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         <img
           width={100}
           src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbY7By1%2FbtqCWLELJ6H%2Fi3EmfldVPdDcMnLiWzzd70%2Fimg.jpg"
