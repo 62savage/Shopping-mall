201584753604fe8092e754f15e536a313cbcd845
62savage <6s2avage@gmail.com>
2024-03-30 18:22:15 +0900

    모바일 화면 일 때 사이드에 메뉴바 출력하기

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar({ authenticate, setAuthenticate }) {
   const navigate = useNavigate();
-  const menuLists = ["여성", "남성", "신생아/유아", "아동", "H&M Home", "Sale"];
+  const menuLists = [
+    "여성",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "지속가능성",
+    "Sale",
+  ];
+
+  const [width, setWidth] = useState(0);
 
   const search = (e) => {
     if (e.key === "Enter") {
@@ -17,37 +27,59 @@ export default function Navbar({ authenticate, setAuthenticate }) {
 
   return (
     <div>
-      <div
-        className="login-button"
-        onClick={() => {
-          if (authenticate) {
-            setAuthenticate(false);
-            return;
-          }
-          navigate("/login");
-        }}
-      >
-        <FontAwesomeIcon icon={faUser} />
-        <div>{authenticate ? "로그아웃" : "로그인"}</div>
-      </div>
-      <div
-        className="nav-section"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        <img
-          width={100}
-          src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbY7By1%2FbtqCWLELJ6H%2Fi3EmfldVPdDcMnLiWzzd70%2Fimg.jpg"
-          alt="logo"
-        />
-      </div>
-      <div className="menu-area">
-        <ul className="menu-list">
+      {/* <div className="side-menu" style={{ width: width }}>
+        <button className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className="side-menu-list" id="menu-list">
           {menuLists.map((list, i) => (
-            <li key={i}>{list}</li>
+            <button key={i}>{list}</button>
+          ))}
+        </div>
+      </div> */}
+
+      <div className="nav-header">
+        <div className="burger-menu hide">
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+        </div>
+
+        <div
+          onClick={() => {
+            if (authenticate) {
+              setAuthenticate(false);
+              return;
+            }
+            navigate("/login");
+          }}
+        >
+          <FontAwesomeIcon icon={faUser} />
+          <span style={{ cursor: "pointer" }}>
+            {authenticate ? "로그아웃" : "로그인"}
+          </span>
+        </div>
+      </div>
+
+      <div className="nav-logo">
+        <Link to="/">
+          <img
+            width={100}
+            src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbY7By1%2FbtqCWLELJ6H%2Fi3EmfldVPdDcMnLiWzzd70%2Fimg.jpg"
+            alt="logo"
+          />
+        </Link>
+      </div>
+
+      <div className="nav-menu-area">
+        <ul className="menu">
+          {menuLists.map((list, i) => (
+            <li>
+              <a href="#" key={i}>
+                {list}
+              </a>
+            </li>
           ))}
         </ul>
+
         <div className="search-box">
           <FontAwesomeIcon icon={faSearch} />
           <input type="text" className="" onKeyDown={search} />
